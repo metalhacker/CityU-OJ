3ff61997e760bf92aeb84562f57f5e878395ca34
metalhacker <32122184+metalhacker@users.noreply.github.com>
2018-11-11 13:34:48 +0800

    Create 001-Sums.cpp

---
+++ 001-Sums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, outNum;
+	while (cin >> num1 >> num2)
+	{
+		outNum = num1 + num2;
+		cout << outNum << endl;
+	}
+	return 0;
+}
